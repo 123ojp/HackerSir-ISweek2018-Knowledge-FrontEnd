a5d62884c8f6b4a5f8ffead68722c6be29a76384
123ojp <shepherd123ojp@gmail.com>
2018-12-26 00:17:31 +0800

    fix rank js

--- assets/js/rank.js
+++ assets/js/rank.js
@@ -4,8 +4,16 @@ function getScore() {
      url: "/getRanking", // 填入網路應用程式網址
      success: function(j) {
       $("#firstplace").text(j[0]['nickname']);
-      $("#secendplace").text(j[1]['nickname']);
-      $("#thridplace").text(j[2]['nickname']);
+      if (j.length < 2){
+        $("#secendplace").text(j[1]['nickname']);
+      } else {
+        $("#secendplace").hide();
+      }
+      if (j.length < 3){
+        $("#thridplace").text(j[2]['nickname']);
+      } else {
+        $("#thridplace").hide();
+      }
        for (i = 1 ; i<=j.length ; i++ ) {
          $("#name"+i).text(j[i-1]['nickname']);
          $("#score"+i).text(j[i-1]['ranking']);
